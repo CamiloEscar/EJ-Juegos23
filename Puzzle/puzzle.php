457b0c9fcfa1dff5af4c1987b9e62546a62b0cbc
camilo escar <camiloescar1995@gmail.com>
2023-09-05 20:45:41 -0300

    Your commit message here

---
+++ Puzzle/puzzle.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puzzle</title>
+</head>
+<h3>asd</h3>
+<body>
+    <center>
+        <h3>Puzzle</h3>
+
+        <table>
+            <tr>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+            </tr>
+            <tr>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+            </tr>
+            <tr>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+                <td><img src="/imagenes/img_0.jpg"/></td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
